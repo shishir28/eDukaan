3835cfd527d4bc92b2f7b93496edcde1d3673806
shishir28 <shishir28@gmail.com>
2018-10-11 09:13:09 +1100

    Basic API gateway  setup

---
+++ src/Services/ApiGateways/Monad.Service.ApiGatewayBase/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace Monad.Service.ApiGatewayBase
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+             WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://*:9000")
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config
+                    .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                    .AddJsonFile("appsettings.json", true, true)
+                    .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+                    .AddJsonFile("ocelot.json")
+                    .AddEnvironmentVariables();
+                })
+                .ConfigureServices(svc =>
+                {
+                    svc.AddOcelot();
+                })
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    //add your logging
+                })
+                .UseIISIntegration()
+                .Configure(con =>
+                {
+                    con.UseOcelot().Wait();
+                });
+    }
+}
