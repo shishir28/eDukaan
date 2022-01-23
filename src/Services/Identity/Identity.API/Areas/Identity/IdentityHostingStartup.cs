cc6fd9abeb849c25f67541f83c40c34cf21f120a
Shishir Mishra <shishir28@gmail.com>
2022-01-24 10:22:34 +1100

    Existing IDP code copied to eDukaan

---
+++ src/Services/Identity/Identity.API/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Identity.API.Areas.Identity.Data;
+using Identity.API.Data;
+
+[assembly: HostingStartup(typeof(Identity.API.Areas.Identity.IdentityHostingStartup))]
+namespace Identity.API.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) =>
+            {
+                services.AddDbContext<IdentityContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("IdentityContextConnection")));
+
+                //services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                //    .AddEntityFrameworkStores<IdentityContext>()
+                //    .AddDefaultTokenProviders();
+
+                services.AddIdentity<ApplicationUser, IdentityRole>()
+               .AddEntityFrameworkStores<IdentityContext>()
+               .AddDefaultTokenProviders()
+               ;
+            });
+        }
+    }
+}
---
+++ src/Services/Identity/Identity.API/Config.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace Identity.API
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                  new IdentityResources.Email(),
+                new IdentityResource("country", new [] { "country" })
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+               new ApiScope("pieshophrapi",
+                    "Pie Shop HR API",
+                    new [] { "country" })
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+              
+                // interactive client using code flow + pkce
+                new Client
+                {
+                    RequireConsent = false,
+                    RequirePkce = true,
+                    ClientId = "PieShopHRM",
+                    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris = { "https://localhost:5002/signin-oidc" }, // URL of client apps
+                    FrontChannelLogoutUri = "https://localhost:5002/signout-oidc",
+                    PostLogoutRedirectUris = { "https://localhost:5002/signout-callback-oidc" },
+                    AllowOfflineAccess = true,
+                    AccessTokenLifetime = 120,
+                    AllowedScopes = { "openid", "profile", "email", "pieshophrapi" }
+                },
+            };
+    }
+}
