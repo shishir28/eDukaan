9962d80190527d1faf7ba6aa0caebacb15150145
Shishir Mishra <shishir28@gmail.com>
2022-01-24 13:35:06 +1100

    upgraded to .net 5

--- src/Services/Identity/Identity.API/Config.cs
+++ src/Services/Identity/Identity.API/Config.cs
@@ -2,6 +2,8 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityModel;
+using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
 
